f9d9650a8f2dd09d946f6779fecf6c75b2ea8a60
CEYLAN EREN <eren-mustafa.ceylan@etu.unistra.fr>
2021-05-11 16:00:03 +0200

    Database.cs: Création du fichier, Connection avec la base de test (bdEvents), Méthode FetchEvents et FetchParticipant pour récuperer toutes les données de la table Evenements et Participants
    Participant.cs: Création du fichier, Ajout des attributs présent dans la base de donnée dans la table Participants
    
    PartyEvent.cs: Création du fichier, Ajout des attributs présent dans la base de donnée dans la table Evenements

---
+++ Hermes/Database.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Hermes
+{
+    class Database
+    {
+        string chcon = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source='X:\\a21 sterne pie\\Base de données\\bdEvents.mdb'";
+        OleDbConnection connection = new OleDbConnection();
+
+        public List<PartyEvent> FetchEvents()
+        {
+            List<PartyEvent> partyEvents = new List<PartyEvent>();
+
+            try
+            {
+                connection.ConnectionString = chcon;
+                connection.Open();
+                OleDbCommand command = new OleDbCommand("select * from Evenements", connection);
+                OleDbDataReader dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    PartyEvent oneEvent = new PartyEvent()
+                    {
+                        CodeEvent = dataReader.GetInt32(0),
+                        TitleEvent = dataReader.GetString(1),
+                        BeginDate = dataReader.GetDateTime(2),
+                        EndDate = dataReader.GetDateTime(3),
+                        Description = dataReader.GetString(4),
+                        BalanceYN = dataReader.GetBoolean(5),
+                        CodeCreator = dataReader.GetInt32(6)
+                    };
+                    partyEvents.Add(oneEvent);
+                }
+            }
+            catch (OleDbException er)
+            {
+                MessageBox.Show("Erreur de requête SQL \n\n\n\n" + er);
+            }
+            catch (InvalidOperationException er)
+            {
+                MessageBox.Show("Problème d'accès à la base \n\n\n\n" + er);
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+            return partyEvents;
+        }
+
+        public List<Participant> FetchParticipant()
+        {
+            List<Participant> participants = new List<Participant>();
+
+            try
+            {
+                connection.ConnectionString = chcon;
+                connection.Open();
+                OleDbCommand command = new OleDbCommand("select * from Participants", connection);
+                OleDbDataReader dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    Participant oneParticipant = new Participant()
+                    {
+                        CodeParticipant = dataReader.GetInt32(0),
+                        FirstName = dataReader.GetString(1),
+                        LastName = dataReader.GetString(2),
+                        PhoneNumber = dataReader.GetInt32(3),
+                        nbParts = dataReader.GetInt32(4),
+                        Balance = dataReader.GetInt32(5),
+                        Mail = dataReader.GetString(6)
+                    };
+                    participants.Add(oneParticipant)
+                }
+            }
+            catch (OleDbException er)
+            {
+                MessageBox.Show("Erreur de requête SQL \n\n\n\n" + er);
+            }
+            catch (InvalidOperationException er)
+            {
+                MessageBox.Show("Problème d'accès à la base \n\n\n\n" + er);
+            }
+
+            finally
+            {
+                connection.Close();
+            }
+            return participants;
+        }
+    }
+}
