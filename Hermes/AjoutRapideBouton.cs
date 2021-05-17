18325ad3884e1a63d96328195ae96e470ff9effa
YANOVS-KYY ALEXANDER <alexander.yanovs-kyy@etu.unistra.fr>
2021-05-17 22:41:52 +0200

    Implémentations AjoutRapideBouton.cs pour Accueil.cs
    Correction dans Database.cs de chemin d'accès de la base de donnée

---
+++ Hermes/AjoutRapideBouton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Hermes
+{
+    public partial class AjoutRapideBouton : UserControl
+    {
+
+        public AjoutRapideBouton()
+        {
+            InitializeComponent();
+        }
+
+        private void AjoutRapideBouton_Load(object sender, EventArgs e)
+        {
+            this.cbbEvenements.Size = new Size(245, 30);
+        }
+    }
+}
