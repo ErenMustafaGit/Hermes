c6f5381cc2e74fb8e3012fbb8ca02145582ec8f0
squid <squid@nohost>
2021-05-17 21:59:48 +0200

    UI: Implemented font manager, AppFontLabel to use custom loaded fonts, font-based icons to use as vectorized icons, enabled High-DPI support

--- Hermes/Program.cs
+++ Hermes/Program.cs
@@ -8,14 +8,25 @@ namespace Hermes
 {
     static class Program
     {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            // High-DPI support for modern Windows versions.
+            if (Environment.OSVersion.Version.Major >= 6)
+                SetProcessDPIAware();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            // Preload required resources for the application.
+            FontManager.PreloadFontFamilies();
+
             Application.Run(new MainForm());
         }
     }
---
+++ Hermes/UI/Icons.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hermes.UI
+{
+    public static class Icons
+    {
+        // Based on the generated font. Must be updated if the icon font is updated.
+        // Special glyphs are used for each icon.
+        public const string HOME = "\uE801";
+        public const string MONEY_BAG = "\uE802";
+        public const string CLIPBOARD = "\uE803";
+        public const string GROUP = "\uE804";
+        public const string CALENDAR_2 = "\uE805";
+        public const string GEAR = "\uE806";
+        public const string RIGHT_ARROW = "\uE807";
+        public const string CALENDAR_1 = "\uE808";
+    }
+}
