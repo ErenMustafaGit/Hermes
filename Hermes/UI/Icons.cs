c5128d73d6c14c1a5aec6b00e3c09d8ec53bc365
squid <squid@nohost>
2021-05-17 22:19:07 +0200

    Icons: update font and associated glyphs

--- Hermes/UI/Icons.cs
+++ Hermes/UI/Icons.cs
@@ -10,13 +10,30 @@ namespace Hermes.UI
     {
         // Based on the generated font. Must be updated if the icon font is updated.
         // Special glyphs are used for each icon.
-        public const string HOME = "\uE801";
-        public const string MONEY_BAG = "\uE802";
-        public const string CLIPBOARD = "\uE803";
-        public const string GROUP = "\uE804";
-        public const string CALENDAR_2 = "\uE805";
-        public const string GEAR = "\uE806";
-        public const string RIGHT_ARROW = "\uE807";
         public const string CALENDAR_1 = "\uE808";
+        public const string CALENDAR_2 = "\uE818";
+
+        public const string RIGHT_ARROW = "\uE807";
+
+        public const string LEFT = "\uE80B";
+        public const string RIGHT = "\uE81D";
+        public const string DOWN = "\uE811";
+
+        public const string EMOTE_THUMBS_UP = "\uE800";
+        public const string LOCATION = "\uE801";
+        public const string MONEY_BAG = "\uE809";
+        public const string MAIL = "\uE80A";
+        public const string HOME = "\uE80D";
+        public const string GROUP = "\uE80E";
+        public const string GEAR = "\uE80F";
+        public const string FINANCE = "\uE810";
+        public const string CROWN = "\uE812";
+        public const string COINS = "\uE813";
+        public const string CLOSE = "\uE814";
+        public const string CLOCK = "\uE815";
+        public const string CLIPBOARD = "\uE816";
+        public const string USER_GROUP = "\uE81A";
+        public const string USER = "\uE81C";
+        public const string PHONE = "\uE81F";
     }
 }
