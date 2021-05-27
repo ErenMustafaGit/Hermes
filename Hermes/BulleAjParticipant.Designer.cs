a35040e0380faf2f4f252f8479a58ed2d65e1db4
CEYLAN EREN <eren-mustafa.ceylan@etu.unistra.fr>
2021-05-27 20:57:58 +0200

    BulleAjParticipant: Verification que les champs soit bien rempli + Creation du nouveau participant à inviter (attente de l'ajout de DataBase.InsertParticipant) + Forcing pour le champ téléphone d'avoir seulement des nombres
    BulleAjParticipant.Designer : Ajout du nombre de part

--- Hermes/BulleAjParticipant.Designer.cs
+++ Hermes/BulleAjParticipant.Designer.cs
@@ -30,15 +30,18 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.btnValider = new System.Windows.Forms.Button();
-            this.txtNom = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.txtMail = new System.Windows.Forms.TextBox();
             this.lblEmail = new Hermes.UI.AppFontLabel();
             this.lblNumero = new Hermes.UI.AppFontLabel();
             this.lblPrenom = new Hermes.UI.AppFontLabel();
             this.lblAjoutDunParticipant = new Hermes.UI.AppFontLabel();
             this.lblNom = new Hermes.UI.AppFontLabel();
+            this.nudNbPart = new System.Windows.Forms.NumericUpDown();
+            this.lblNbPart = new Hermes.UI.AppFontLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNbPart)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -46,7 +49,7 @@
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.SystemColors.Control;
-            this.button1.Location = new System.Drawing.Point(381, 330);
+            this.button1.Location = new System.Drawing.Point(381, 351);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(96, 30);
             this.button1.TabIndex = 31;
@@ -59,55 +62,57 @@
             this.btnValider.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(147)))), ((int)(((byte)(248)))));
             this.btnValider.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnValider.ForeColor = System.Drawing.SystemColors.Control;
-            this.btnValider.Location = new System.Drawing.Point(492, 330);
+            this.btnValider.Location = new System.Drawing.Point(492, 351);
             this.btnValider.Name = "btnValider";
             this.btnValider.Size = new System.Drawing.Size(96, 30);
             this.btnValider.TabIndex = 32;
             this.btnValider.Text = "Valider";
             this.btnValider.UseVisualStyleBackColor = false;
+            this.btnValider.Click += new System.EventHandler(this.btnValider_Click);
             // 
-            // txtNom
+            // txtFirstName
             // 
-            this.txtNom.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtNom.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.txtNom.Location = new System.Drawing.Point(132, 174);
-            this.txtNom.Name = "txtNom";
-            this.txtNom.Size = new System.Drawing.Size(201, 27);
-            this.txtNom.TabIndex = 23;
+            this.txtFirstName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.txtFirstName.Location = new System.Drawing.Point(132, 147);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(201, 27);
+            this.txtFirstName.TabIndex = 23;
             // 
-            // textBox1
+            // txtLastName
             // 
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.textBox1.Location = new System.Drawing.Point(132, 257);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(201, 27);
-            this.textBox1.TabIndex = 34;
+            this.txtLastName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.txtLastName.Location = new System.Drawing.Point(132, 230);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(201, 27);
+            this.txtLastName.TabIndex = 34;
             // 
-            // textBox2
+            // txtPhoneNumber
             // 
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.textBox2.Location = new System.Drawing.Point(381, 174);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(201, 27);
-            this.textBox2.TabIndex = 36;
+            this.txtPhoneNumber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPhoneNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.txtPhoneNumber.Location = new System.Drawing.Point(381, 147);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(201, 27);
+            this.txtPhoneNumber.TabIndex = 36;
+            this.txtPhoneNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPhoneNumber_KeyPress);
             // 
-            // textBox3
+            // txtMail
             // 
-            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.textBox3.Location = new System.Drawing.Point(381, 257);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(201, 27);
-            this.textBox3.TabIndex = 38;
+            this.txtMail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtMail.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.txtMail.Location = new System.Drawing.Point(381, 230);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(201, 27);
+            this.txtMail.TabIndex = 38;
             // 
             // lblEmail
             // 
             this.lblEmail.AppFont = Hermes.AppFont.HelveticaNeue;
             this.lblEmail.AppFontHeight = 10F;
             this.lblEmail.AutoSize = true;
-            this.lblEmail.Location = new System.Drawing.Point(378, 228);
+            this.lblEmail.Location = new System.Drawing.Point(378, 201);
             this.lblEmail.Name = "lblEmail";
             this.lblEmail.Size = new System.Drawing.Size(115, 16);
             this.lblEmail.TabIndex = 37;
@@ -118,7 +123,7 @@
             this.lblNumero.AppFont = Hermes.AppFont.HelveticaNeue;
             this.lblNumero.AppFontHeight = 10F;
             this.lblNumero.AutoSize = true;
-            this.lblNumero.Location = new System.Drawing.Point(378, 145);
+            this.lblNumero.Location = new System.Drawing.Point(378, 118);
             this.lblNumero.Name = "lblNumero";
             this.lblNumero.Size = new System.Drawing.Size(167, 16);
             this.lblNumero.TabIndex = 35;
@@ -129,7 +134,7 @@
             this.lblPrenom.AppFont = Hermes.AppFont.HelveticaNeue;
             this.lblPrenom.AppFontHeight = 10F;
             this.lblPrenom.AutoSize = true;
-            this.lblPrenom.Location = new System.Drawing.Point(129, 228);
+            this.lblPrenom.Location = new System.Drawing.Point(129, 201);
             this.lblPrenom.Name = "lblPrenom";
             this.lblPrenom.Size = new System.Drawing.Size(170, 16);
             this.lblPrenom.TabIndex = 33;
@@ -140,7 +145,7 @@
             this.lblAjoutDunParticipant.AppFont = Hermes.AppFont.HelveticaNeue_Bold;
             this.lblAjoutDunParticipant.AppFontHeight = 24F;
             this.lblAjoutDunParticipant.AutoSize = true;
-            this.lblAjoutDunParticipant.Location = new System.Drawing.Point(175, 69);
+            this.lblAjoutDunParticipant.Location = new System.Drawing.Point(175, 51);
             this.lblAjoutDunParticipant.Name = "lblAjoutDunParticipant";
             this.lblAjoutDunParticipant.Size = new System.Drawing.Size(339, 40);
             this.lblAjoutDunParticipant.TabIndex = 30;
@@ -151,30 +156,62 @@
             this.lblNom.AppFont = Hermes.AppFont.HelveticaNeue;
             this.lblNom.AppFontHeight = 10F;
             this.lblNom.AutoSize = true;
-            this.lblNom.Location = new System.Drawing.Point(129, 145);
+            this.lblNom.Location = new System.Drawing.Point(129, 118);
             this.lblNom.Name = "lblNom";
             this.lblNom.Size = new System.Drawing.Size(150, 16);
             this.lblNom.TabIndex = 22;
             this.lblNom.Text = "Nom du participant ? *";
             // 
+            // nudNbPart
+            // 
+            this.nudNbPart.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.nudNbPart.Location = new System.Drawing.Point(132, 313);
+            this.nudNbPart.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudNbPart.Name = "nudNbPart";
+            this.nudNbPart.Size = new System.Drawing.Size(201, 27);
+            this.nudNbPart.TabIndex = 39;
+            this.nudNbPart.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lblNbPart
+            // 
+            this.lblNbPart.AppFont = Hermes.AppFont.HelveticaNeue;
+            this.lblNbPart.AppFontHeight = 10F;
+            this.lblNbPart.AutoSize = true;
+            this.lblNbPart.Location = new System.Drawing.Point(129, 284);
+            this.lblNbPart.Name = "lblNbPart";
+            this.lblNbPart.Size = new System.Drawing.Size(129, 16);
+            this.lblNbPart.TabIndex = 33;
+            this.lblNbPart.Text = "Nombre de part ? *";
+            // 
             // BulleAjParticipant
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.nudNbPart);
+            this.Controls.Add(this.txtMail);
             this.Controls.Add(this.lblEmail);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtPhoneNumber);
             this.Controls.Add(this.lblNumero);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.lblNbPart);
             this.Controls.Add(this.lblPrenom);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnValider);
             this.Controls.Add(this.lblAjoutDunParticipant);
-            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.txtFirstName);
             this.Controls.Add(this.lblNom);
             this.Name = "BulleAjParticipant";
             this.Size = new System.Drawing.Size(705, 405);
             this.Load += new System.EventHandler(this.BulleAjParticipant_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudNbPart)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -185,13 +222,15 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button btnValider;
         private UI.AppFontLabel lblAjoutDunParticipant;
-        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.TextBox txtFirstName;
         private UI.AppFontLabel lblNom;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtLastName;
         private UI.AppFontLabel lblPrenom;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
         private UI.AppFontLabel lblNumero;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtMail;
         private UI.AppFontLabel lblEmail;
+        private System.Windows.Forms.NumericUpDown nudNbPart;
+        private UI.AppFontLabel lblNbPart;
     }
 }
--- Hermes/BulleAjParticipant.cs
+++ Hermes/BulleAjParticipant.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Hermes.DataModel;
 
 namespace Hermes
 {
@@ -33,6 +34,58 @@ namespace Hermes
         private void Button1_Click(object sender, EventArgs e)
         {
             annuler.DynamicInvoke();
+        }
+
+        private void btnValider_Click(object sender, EventArgs e)
+        {
+            bool valide = true;
+            if (txtFirstName.Text.Length < 2)
+            {
+                //AVEC DES ALERTES SUR LES CHAMPS NON REMPLIS
+                valide = false;
+            }
+            if (txtLastName.Text.Length < 2)
+            {
+                valide = false;
+            }
+            if (txtMail.Text.Length < 8 && !txtMail.Text.Contains("@"))
+            {
+                valide = false;
+            }
+
+            if (txtPhoneNumber.Text.Length < 8)
+            {
+                valide = false;
+            }
+
+
+           
+            if (valide)
+            {
+
+                //Creation du nouveau participant
+                Participant newParticipant = new Participant()
+                {
+                    CodeParticipant = 0,
+                    LastName = txtLastName.Text,
+                    FirstName = txtFirstName.Text,
+                    PhoneNumber = txtPhoneNumber.Text,
+                    NbParts = int.Parse(txtPhoneNumber.Text),
+                    //Balance = null,
+                    Mail = txtMail.Text,
+                };
+
+                //Database.InsertParticipant(newParticipant)
+            }
+        }
+
+        private void txtPhoneNumber_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+            if (char.IsDigit(e.KeyChar))
+            {
+                e.Handled = false;
+            }
         }
     }
 }
