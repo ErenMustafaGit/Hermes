e68ffee94495bf30d0d9bad02b76b73601b88284
CEYLAN EREN <eren-mustafa.ceylan@etu.unistra.fr>
2021-05-27 11:12:55 +0200

    BulleInvitation.cs: RefreshParticipants() permet d'avoir une checkBox pour chaque participant BulleInvitations.Designer: Suppression de l'appartition systémique du checkBox "Tout le monde", il apparaitra seulement si il y a des participants à inviter

--- Hermes/BulleInvitations.Designer.cs
+++ Hermes/BulleInvitations.Designer.cs
@@ -32,9 +32,7 @@
             this.btnValider = new System.Windows.Forms.Button();
             this.lblInvitations = new Hermes.UI.AppFontLabel();
             this.cboEvenements = new System.Windows.Forms.ComboBox();
-            this.pnlBeneficiaire = new System.Windows.Forms.Panel();
-            this.chkEveryOne = new System.Windows.Forms.CheckBox();
-            this.pnlBeneficiaire.SuspendLayout();
+            this.pnlParticipant = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // button1
@@ -82,31 +80,20 @@
             this.cboEvenements.Name = "cboEvenements";
             this.cboEvenements.Size = new System.Drawing.Size(221, 28);
             this.cboEvenements.TabIndex = 28;
+            this.cboEvenements.SelectionChangeCommitted += new System.EventHandler(this.CboEvenements_SelectionChangeCommitted);
             // 
-            // pnlBeneficiaire
+            // pnlParticipant
             // 
-            this.pnlBeneficiaire.Controls.Add(this.chkEveryOne);
-            this.pnlBeneficiaire.Location = new System.Drawing.Point(66, 122);
-            this.pnlBeneficiaire.Name = "pnlBeneficiaire";
-            this.pnlBeneficiaire.Size = new System.Drawing.Size(557, 187);
-            this.pnlBeneficiaire.TabIndex = 33;
-            // 
-            // chkEveryOne
-            // 
-            this.chkEveryOne.AutoSize = true;
-            this.chkEveryOne.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.chkEveryOne.Location = new System.Drawing.Point(21, 14);
-            this.chkEveryOne.Name = "chkEveryOne";
-            this.chkEveryOne.Size = new System.Drawing.Size(135, 22);
-            this.chkEveryOne.TabIndex = 0;
-            this.chkEveryOne.Text = "Tout le monde";
-            this.chkEveryOne.UseVisualStyleBackColor = true;
+            this.pnlParticipant.Location = new System.Drawing.Point(66, 122);
+            this.pnlParticipant.Name = "pnlParticipant";
+            this.pnlParticipant.Size = new System.Drawing.Size(557, 187);
+            this.pnlParticipant.TabIndex = 33;
             // 
             // BulleInvitations
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.pnlBeneficiaire);
+            this.Controls.Add(this.pnlParticipant);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnValider);
             this.Controls.Add(this.lblInvitations);
@@ -114,8 +101,6 @@
             this.Name = "BulleInvitations";
             this.Size = new System.Drawing.Size(705, 405);
             this.Load += new System.EventHandler(this.BulleInvitations_Load);
-            this.pnlBeneficiaire.ResumeLayout(false);
-            this.pnlBeneficiaire.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,7 +112,6 @@
         private System.Windows.Forms.Button btnValider;
         private UI.AppFontLabel lblInvitations;
         private System.Windows.Forms.ComboBox cboEvenements;
-        private System.Windows.Forms.Panel pnlBeneficiaire;
-        private System.Windows.Forms.CheckBox chkEveryOne;
+        private System.Windows.Forms.Panel pnlParticipant;
     }
 }
--- Hermes/BulleInvitations.cs
+++ Hermes/BulleInvitations.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Hermes.DataModel;
 
 namespace Hermes
 {
@@ -32,13 +33,64 @@ namespace Hermes
 
         private void BulleInvitations_Load(object sender, EventArgs e)
         {
+            List<PartyEvent> events = Database.FetchEvents();
+            cboEvenements.DataSource = events.ToDataTable();
+            cboEvenements.DisplayMember = "Name";
+            cboEvenements.ValueMember = "Id";
 
+            RefreshParticipants();
+           
+            
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
             pnlBulle.Controls.Clear();
             pnlBulle.Visible = false;
+        }
+
+        public void RefreshParticipants()
+        {
+            pnlParticipant.Controls.Clear();
+
+            PartyEvent currentEvent = PartyEvent.GetFromId((int)cboEvenements.SelectedValue);
+            //List<Participant> nonInvitedParticipants = currentEvent.GetNonInvitedParticipant() -> Christo en train de le faire
+
+            List<Participant> nonInvitedParticipants = Database.FetchParticipant();
+
+            if (nonInvitedParticipants.Count != 0)
+            {
+                CheckBox chkEveryOne = new CheckBox();
+                chkEveryOne.Text = "Tout le monde";
+                chkEveryOne.AutoSize = true;
+                chkEveryOne.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Bold);
+                chkEveryOne.Top = 20;
+                chkEveryOne.Left = 20;
+                pnlParticipant.Controls.Add(chkEveryOne);
+
+                int modulo = 2;
+                for (int i = 0; i < nonInvitedParticipants.Count; i++)
+                {
+                    CheckBox chk = new CheckBox();
+                    chk.Text = nonInvitedParticipants[i].LastName + " " + nonInvitedParticipants[i].FirstName;
+                    chk.AutoSize = true;
+                    chk.Font = new Font("Microsoft Sans Serif", 11.25F);
+                    chk.Top = 40 + 20 * (i / modulo);
+                    chk.Left = 20 + 300 * (i % modulo);
+                    pnlParticipant.Controls.Add(chk);
+                }
+            }
+            else
+            {
+                //Affichage pour indiquer que tout le monde est déjà invité
+            }
+
+        }
+
+        private void CboEvenements_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            RefreshParticipants();
         }
     }
 }
