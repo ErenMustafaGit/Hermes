5ba1d56410f64b8fec07dc94eb135d7c6ea66d5d
YANOVS-KYY ALEXANDER <alexander.yanovs-kyy@etu.unistra.fr>
2021-05-27 10:13:03 +0200

    Ajout de la partie Bilan: mise en place des combobox Participant et Evenement, mise en place du usercontrol pour afficher une dépense (que j'ai setup). Manque à créé le user control pour les remboursement et plus qu'à utilsier la procédure stockée :)

---
+++ Hermes/Bilan.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Hermes.DataModel;
+
+namespace Hermes
+{
+    public partial class Bilan : UserControl
+    {
+        public Panel ecran;
+        public Panel setPanel
+        {
+            set { this.ecran = value; }
+        }
+        public Bilan()
+        {
+            InitializeComponent();
+        }
+
+        private void Bilan_Load(object sender, EventArgs e)
+        {
+            List<Participant> listeParticipants = Database.FetchParticipant();
+            DataTable dataTableParticipants = Participant.toConcatenateDataTable(listeParticipants);
+            cboParticipant.DataSource = dataTableParticipants;
+            cboParticipant.DisplayMember = "Name";
+            cboParticipant.ValueMember = "CodeParticipant";
+
+            DataTable dataTableEvent = Database.FetchEvents().ToDataTable();
+            cboEvent.DataSource = dataTableEvent;
+            cboEvent.DisplayMember = "Name";
+            cboEvent.ValueMember = "Id";
+        }
+
+        private void depenseUser1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
