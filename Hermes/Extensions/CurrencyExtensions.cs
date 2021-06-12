06de159dbba414425cd6acc747dc98eafe9f0a69
squid <squid@nohost>
2021-06-12 23:39:19 +0200

    Extensions: extend floating point types and DateTime for locale reasons

---
+++ Hermes/Extensions/CurrencyExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hermes.Extensions
+{
+    static class CurrencyExtensions
+    {
+        private static CultureInfo frFR = new CultureInfo("fr-FR");
+
+        public static string ToEuros(this double v)
+        {
+            return v.ToString("C", frFR);
+        }
+
+        public static string ToEuros(this float v)
+        {
+            return v.ToString("C", frFR);
+        }
+
+        public static string ToEuros(this decimal v)
+        {
+            return v.ToString("C", frFR);
+        }
+    }
+}
---
+++ Hermes/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hermes.Extensions
+{
+    static class DateTimeExtensions
+    {
+        private static CultureInfo frFR = new CultureInfo("fr-FR");
+
+        public static string ToFrenchLongDateString(this DateTime dateTime)
+        {
+            return dateTime.ToString("D", frFR);
+        }
+    }
+}
